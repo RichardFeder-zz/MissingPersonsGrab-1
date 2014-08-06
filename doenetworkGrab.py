47a6705e9b345cab371cbccaafefd4129d0fb6f2
RichardFeder <richardfederstaehle@college.harvard.edu>
2014-08-05 21:19:07 -0700

    Update doenetworkGrab.py

--- doenetworkGrab.py
+++ doenetworkGrab.py
@@ -45,3 +45,49 @@ def iterGrab():
         print i, " where we are in the iteration"
     return all_cases
 # iterGrab('http://doenetwork.org/cases/disappear.html')
+
+import requests
+import lxml.html
+
+def NamphGrab(url):
+    r = requests.get(url)
+    html = lxml.html.fromstring(r.text)
+    processed = html.xpath('//a')
+    images = []
+    links = []
+    for elem in processed:
+        if not "index" in elem.values()[0]:
+            if not "mp" in elem.values()[0]:
+                if not "hot" in elem.values()[0]:
+                    if not "submit" in elem.values()[0]:
+                        if not "search" in elem.values()[0]:
+                            if not "contact" in elem.values()[0]:
+                                if not "located" in elem.values()[0]:
+                                    if not "media" in elem.values()[0]:
+                                        if not "about" in elem.values()[0]:
+                                            if not "links" in elem.values()[0]:
+                                                if not "bravenet" in elem.values()[0]:
+                                                    if not "s18" in elem.values()[0]:
+                                                        if not "microsoft" in elem.values()[0]:
+                                                            if not "mailto" in elem.values()[0]:
+                                                                if not "updates" in elem.values()[0]:
+                                                                    if not "Bravenet" in elem.values()[0]:
+                                                                        links.append(elem.values())
+    case_file_urls = []
+    
+    for rel_url in links:
+        case_file_urls.append("http://www.nampn.org/"+rel_url[0])
+    case_data = []
+    for case in case_file_urls:
+        r = requests.get(case)
+        html = lxml.html.fromstring(r.text)
+        case_html = html.xpath('//img')
+        case_images = [elem.values() for elem in case_html]
+        case_images.insert(0,case)
+        case_data.append(case_images)
+    return case_data
+    
+# NamphGrab("http://www.nampn.org/cases")
+    
+                                                
+
